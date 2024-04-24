41c14662073e963a312f30beccb7d1319e21e22f
Ruminov <74424348+Ruminov@users.noreply.github.com>
2024-04-23 23:46:24 -0400

    Refactor script.js and style.css

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
 const container = document.querySelector(".container");
+let gridSize = 16;
 
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement("div");
-  div.classList.add("test");
-  container.appendChild(div);
+function createSquares() {
+  for (let i = 0; i < gridSize; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+    for (let i = 0; i < gridSize; i++) {
+      const column = document.createElement("div");
+      column.classList.add("column");
+      row.appendChild(column);
+    }
+  }
 }
 
-function changeBackground(e) {
-  //   e.target.style.backgroundColor = "blue";
+// function changeSquareColor(e) {
+//   e.target.style.backgroundColor = "#000";
+// }
+
+// container.addEventListener("mousemove", changeSquareColor);
+
+// Clear button
+function clearSquares() {
+  container.innerHTML = "";
+  createSquares();
 }
 
-document.addEventListener("mousedown", changeBackground);
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", clearSquares);
+
+// Resize button
+function resizeSquares() {
+  input = +prompt("Number of squares per side (maximum 100)");
+  if (input && typeof input === "number") {
+    gridSize = input;
+    clearSquares();
+  } else {
+    alert("Insert a valid number");
+  }
+}
+
+const resizeBtn = document.querySelector(".resize");
+resizeBtn.addEventListener("click", resizeSquares);
+
+createSquares();
